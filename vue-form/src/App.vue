c9566a14df6d109a830a5b035e59ff135fe4cc4f
DESKTOP-KB8TG84\Taejun <tjkimin2021@gmail.com>
2021-11-25 20:59:32 +0900

    vue practice

---
+++ vue-form/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <form v-on:submit.prevent="submitForm">
+      <div>
+        <label for="username">id: </label>
+        <input id="username" type="text" v-model="username">
+      </div>
+      <div>
+        <label for="password">password: </label>
+        <input id="password" type="password" v-model="password">
+      </div>
+      <button type="submit">제출 버튼</button>
+      <button type="reset">취소</button>
+    </form>
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+  data: function() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    submitForm: function() {
+      // event.preventDefault();
+      console.log(this.username, this.password);
+      // axois.post();
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
